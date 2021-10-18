2dcaf4a339c532ed6875b580be5c6724d6ba192d
xiatian <819337877@qq.com>
2021-10-18 15:41:24 +0800

    init

---
+++ src/main/java/TDD.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: 夏天
+ * @Date: 2021-10-18 15:39
+ * @Description:
+ */
+public class TDD {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
---
+++ src/test/java/TDDTest.java
@@ -0,0 +1,8 @@
+/**
+ * @Author: 夏天
+ * @Date: 2021-10-18 15:39
+ * @Description:
+ */
+public class TDDTest {
+
+}
